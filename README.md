b388f7119158caf0fb5c85143581b92dea5d0a51
Zoey Hashemi <your@email.address>
2023-11-23 15:14:44 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Name : Zore(Zoey) Hashemi
+# Student number : 041076728
+### Interactive Image Gallery Readme
+
+In This JavaScript code  I power an interactive image gallery with features such as image expansion, download. Below is an overview of the main functionalities:
+
+Image Modal
+The script enables a modal window to appear when clicking on an image. The modal contains the enlarged image along with a close button. The modal functionality is facilitated by the classes modal, modalImage, and modalText.
+Download Image
+Download functionality is implemented through the download icon (.fa-download). When clicked, it creates a temporary link to the image and triggers a download.
+Expand Image
+The script allows users to toggle between an enlarged and regular view of an image by clicking the expand icon (.fa-expand-arrows-alt). This is achieved by adding/removing the class enlarged to the image container.
---
+++ script.js
@@ -0,0 +1,105 @@
+const images = document.querySelectorAll(".images img");
+const modal = document.querySelector(".modal");
+const modalImg = document.querySelector(".modalImage"); // Corrected variable name
+const modalText = document.querySelector(".modalText"); // Corrected variable name
+const close = document.querySelector(".close");
+
+images.forEach(Image => {
+    Image.addEventListener("click", () => {
+        modal.classList.add("appear");
+        modalImg.src = Image.src; // Corrected variable name
+        modalText.innerHTML = Image.alt; // Corrected variable name
+
+        close.addEventListener("click", () => {
+            modal.classList.remove("appear");
+        });
+    });
+});
+document.addEventListener("DOMContentLoaded", function () {
+    const downloadIcons = document.querySelectorAll(".fa-download");
+
+    downloadIcons.forEach(icon => {
+        icon.addEventListener("click", function () {
+            const imageSource = this.closest("div").previousElementSibling.src;
+
+            
+            const downloadLink = document.createElement("a");
+
+           
+            downloadLink.href = imageSource;
+            downloadLink.download = "downloaded_image.jpg";
+
+            
+            document.body.appendChild(downloadLink);
+
+            
+            downloadLink.click();
+
+          
+            document.body.removeChild(downloadLink);
+        });
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    const expandIcons = document.querySelectorAll(".fa-expand-arrows-alt");
+
+    expandIcons.forEach(icon => {
+        icon.addEventListener("click", function () {
+            // Find the parent container of the clicked icon
+            const imageContainer = this.closest("div");
+
+            // Toggle a class to enlarge or shrink the image
+            imageContainer.classList.toggle("enlarged");
+        });
+    });
+
+    const downloadIcons = document.querySelectorAll(".fa-download");
+
+    downloadIcons.forEach(icon => {
+        icon.addEventListener("click", function () {
+            const imageSource = this.closest("div").previousElementSibling.src;
+
+            // Create a link element
+            const downloadLink = document.createElement("a");
+
+            // Set the href and download attributes
+            downloadLink.href = imageSource;
+            downloadLink.download = "downloaded_image.jpg"; // Customize the downloaded filename here
+
+            // Append the link to the body
+            document.body.appendChild(downloadLink);
+
+            // Trigger a click on the link
+            downloadLink.click();
+
+            // Remove the link from the body
+            document.body.removeChild(downloadLink);
+        });
+    });
+});
+document.addEventListener("DOMContentLoaded", function () {
+    const expandIcons = document.querySelectorAll(".fa-expand-arrows-alt");
+    const modal = document.querySelector(".modal");
+    const modalImg = document.querySelector(".modalImage");
+    const close = document.querySelector(".close");
+
+    expandIcons.forEach(icon => {
+        icon.addEventListener("click", function () {
+            const imageSource = this.closest("div").previousElementSibling.src;
+
+            // Set the modal image source
+            modalImg.src = imageSource;
+
+            // Show the modal
+            modal.classList.add("appear");
+
+            // Close modal event
+            close.addEventListener("click", function () {
+                modal.classList.remove("appear");
+            });
+        });
+    });
+});
+
+
